51fbf0116ea4bda8f94c1226debd2ecfd3a061bc
Maria <mtens17@gmail.com>
2022-11-21 15:36:05 +0000

    Include searchCriteria function in app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import episodes from "./episodes.json";
 import { EpisodesMap } from "./components/EpisodeListView";
 import { useState } from "react";
 import { SearchBar } from "./components/SearchBar";
+import { searchCriteria } from "./utils/searchCriteria";
 
 function App(): JSX.Element {
   const [input, setInput] = useState<string>("");
@@ -11,12 +12,14 @@ function App(): JSX.Element {
     setInput(searchInput);
   };
 
+  const filteredEpisodes = searchCriteria(episodes, input)
+
   return (
     <>
       <SearchBar value={input} onChange={handleSearchInput} />
       <p>
         {" "}
-        {episodes.map((episode) => {
+        {filteredEpisodes.map((episode) => {
           return <EpisodesMap episodeInfo={episode} key={episode.id} />;
         })}
       </p>
--- src/utils/searchCriteria.ts
+++ src/utils/searchCriteria.ts
@@ -1,5 +1,7 @@
 import { IEpisode } from "../components/EpisodeListView";
 
-export function searchCriteria(episode: IEpisode): string {
-  return;
+export function searchCriteria(episodes: IEpisode[], searchItem: string): IEpisode[] {
+  return episodes.filter((episode: IEpisode) => {
+    return episode.name.toLowerCase().includes(searchItem.toLowerCase())|| episode.summary.toLowerCase().includes(searchItem.toLowerCase())
+  })
 }
