8ae57dcd4244838508fef8daf3ad0007bb37c759
Maria <mtens17@gmail.com>
2022-12-04 16:05:12 +0000

    Remove b tags now working

--- src/utils/summaryFormat.test.ts
+++ src/utils/summaryFormat.test.ts
@@ -1,8 +1,8 @@
 import { formatSummary } from "./summaryFormat";
-import episodes from "../episodes.json";
+// import episodes from "../episodes.json";
+import shows from "../shows.json"
 
-test("generateEpCode generates the season and episode code following guidelines", () => {
-  expect(formatSummary(episodes[0])).toBe(
-    "Lord Eddard Stark, ruler of the North, is summoned to court by his old friend, King Robert Baratheon, to serve as the King's Hand. Eddard reluctantly agrees after learning of a possible threat to the King's life. Eddard's bastard son Jon Snow must make a painful decision about his own future, while in the distant east Viserys Targaryen plots to reclaim his father's throne, usurped by Robert, by selling his sister in marriage."
+test("remove html tags", () => {
+  expect(formatSummary(shows[2].summary)).toBe("Girl Meets World is based on ABC's hugely popular sitcom, Boy Meets World (1993). Set in New York City, the show tells the wonderfully funny heartfelt stories that Boy Meets World is renowned for - only this time from a tween girl's perspective - as the curious and bright 7th grader Riley Matthews and her quick-witted friend Maya Fox embark on an unforgettable middle school experience. But their plans for a carefree year will be adjusted slightly under the watchful eyes of Riley's parents - dad Cory, who's also a faculty member (and their new History teacher), and mom Topanga, who owns a trendy after school hangout that specializes in pudding.",
   );
 });
--- src/utils/summaryFormat.ts
+++ src/utils/summaryFormat.ts
@@ -1,3 +1,5 @@
-export function formatSummary(summary: string): string {
-  return summary.slice(3, summary.length - 4);
+export function formatSummary(text: string){
+  let text2 = text.replace("<b>", "")
+  text2 = text2.replace("</b>", "")
+  return text2.slice(3, text2.length - 4);
 }
